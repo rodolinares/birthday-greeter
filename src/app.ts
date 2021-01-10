810b8ad03e715bf0d3ed88d4dc46b6ceab88e233
Rodrigo Linares <rodolinares19@gmail.com>
2021-01-09 20:05:12 -0500

    Add connection to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
+// import bluebird from "bluebird";
 import bodyParser from "body-parser";
 import compression from "compression";
 import express from "express";
+import mongoose from "mongoose";
 import nodeHtmlToImage from "node-html-to-image";
 
+import { MONGODB_URI } from "./util/secrets";
+
 const app = express();
+
+const mongoUrl = MONGODB_URI;
+// mongoose.Promise = bluebird;
+
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connection success.");
+  })
+  .catch(error => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${error}`);
+    // process.exit();
+  });
 
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
